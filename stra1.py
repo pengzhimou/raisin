7a140220512f6f94ddede8a66acda76da1beea9f
robinpeng <robinpeng@tencent.com>
2023-10-31 11:39:51 +0800

    x

--- stra1.py
+++ stra1.py
@@ -22,45 +22,45 @@ def doubleMa(*args, **kwargs):
 
 if __name__ == '__main__':
     # 修饰原始数据部分
-    HQDf = pd.read_csv('data/T888_1d.csv',
-                       index_col='date')  # 0 1 2 index换成date这一列作为index
-    
-    # HQDf = pd.read_csv('data/T888_15m.csv',
+    # HQDf = pd.read_csv('data/T888_1d.csv',
     #                    index_col='date')  # 0 1 2 index换成date这一列作为index
+    
+    HQDf = pd.read_csv('data/T888_15m.csv',
+                       index_col='datetime')  # 0 1 2 index换成date这一列作为index
 
     HQDf.index = pd.to_datetime(HQDf.index)  # 转成2020-01-02 补0的格式
 
     ################# part 1
     # # 根据传入的策略进行分析
-    ctaParas = {'fast': 5, 'slow': 10}
-    ResultTSDf, StatDf = CTA(HQDf, 30, doubleMa, **ctaParas)
-    print("-----result1-----")
-    print(StatDf)
-    view(ResultTSDf)
-    # 绘图部分
-    plotResult(ResultTSDf)
-    ResultTSDf.to_csv('data/new.csv')
+    # ctaParas = {'fast': 5, 'slow': 10}
+    # ResultTSDf, StatDf = CTA(HQDf, 30, doubleMa, **ctaParas)
+    # print("-----result1-----")
+    # print(StatDf)
+    # # view(ResultTSDf)
+    # # 绘图部分
+    # plotResult(ResultTSDf)
+    # ResultTSDf.to_csv('data/new1.csv')
 
     # ################# part 2
-    # # # sapce 是参数空间，定义贝叶斯搜索的空间
-    # # # func 技术指标名称
-    # # # fast slow 为技术指标的参数范围
-    # space = {
-    #         "HQDf": HQDf,
-    #         "loadBars": 40,
-    #         "func": doubleMa,
-    #         "fast": hp.quniform("fast", 3, 30, 1),
-    #         "slow": hp.quniform("slow", 5, 40, 1),
-    #     }
+    # # sapce 是参数空间，定义贝叶斯搜索的空间
+    # # func 技术指标名称
+    # # fast slow 为技术指标的参数范围
+    space = {
+            "HQDf": HQDf,
+            "loadBars": 40,
+            "func": doubleMa,
+            "fast": hp.quniform("fast", 3, 30, 1),
+            "slow": hp.quniform("slow", 5, 40, 1),
+        }
 
-    # # 调用贝叶斯搜索，第一个参数为参数空间，第二个为优化目标（求解优化目标极值）
-    # trials, best = hypeFun(space, 'sharpe_ratio')
+    # 调用贝叶斯搜索，第一个参数为参数空间，第二个为优化目标（求解优化目标极值）
+    trials, best = hypeFun(space, 'sharpe_ratio')
 
-    # BestResultTSDf, BestStatDf = CTA(HQDf, 30, doubleMa, **best)
-    # print("-----result-----")
-    # print(BestStatDf)
-    # view(ResultTSDf)
+    BestResultTSDf, BestStatDf = CTA(HQDf, 30, doubleMa, **best)
+    print("-----result-----")
+    print(BestStatDf)
+    view(BestResultTSDf)
 
-    # # 绘图部分
-    # plotResult(BestResultTSDf)
-    # ResultTSDf.to_csv('data/new.csv')
+    # 绘图部分
+    plotResult(BestResultTSDf)
+    BestResultTSDf.to_csv('data/new2.csv')
